facfee5dc928c6a81aa1195957424b4d030231cd
dpaweloszek <dawidpaweloszek99@gmail.com>
2023-01-25 13:58:00 +0100

    Update with some changes in structurte

--- Assets/Scripts/BasicSubmarine.cs
+++ Assets/Scripts/BasicSubmarine.cs
@@ -7,11 +7,21 @@ using UnityEngine.InputSystem.DualShock;
 public class BasicSubmarine : MonoBehaviour
 {
     public Color idleColor;
+    public Player input;
+    public float speed;
 
     private void Awake()
     {
         var gamepad = (DualShockGamepad)Gamepad.all[0];
         gamepad.SetLightBarColor(idleColor);
+
+        input = GetComponent<Player>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Vector2 inputVector = input.Movement.Up&Down.ReadValue<Vector2>();
+        //transform.position += new Vector3(inputVector.x, 0, inputVector.y) * Time.deltaTime * speed;
     }
 
     //// Update is called once per frame
